fef554ecf0b59669cc7a12437b15052eade3e7ed
Nguyễn Phúc Thuần <phucthuan1st@gmail.com>
2022-05-23 22:52:34 +0700

    Hello Java
    beginner, var, user input and expression

---
+++ HelloJava/src/Main.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		
+		// TODO Auto-generated method stub
+		System.out.println("Hello Java!");
+		System.out.println("My name is Thuan");
+		System.out.print("Java ask: \"What is your age: ");
+		
+		int age = scanner.nextInt();
+		
+		System.out.println("My age is: " + age);
+		
+	}
+
+}
